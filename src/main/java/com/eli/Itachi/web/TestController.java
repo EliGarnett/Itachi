28828ff953b1175884e430be0fe16b9954938430
chenlu <chenlu@wepiao.com>
2018-03-22 20:03:02 +0800

    this is master second commit

--- src/main/java/com/eli/Itachi/web/TestController.java
+++ src/main/java/com/eli/Itachi/web/TestController.java
@@ -5,5 +5,6 @@ public class TestController {
     public static void main(String[] args) {
         System.out.println("this is master");
         System.out.println("this is master first commit");
+        System.out.println("this is master second commit");
     }
 }
