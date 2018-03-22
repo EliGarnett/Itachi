385486f617354030c0a8dbb8606eaec56bf94192
chenlu <chenlu@wepiao.com>
2018-03-22 19:53:20 +0800

    init

---
+++ src/main/java/com/eli/Itachi/ItachiApplication.java
@@ -0,0 +1,12 @@
+package com.eli.Itachi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItachiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ItachiApplication.class, args);
+  }
+}
