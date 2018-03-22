93ee15f07275280aeea6bf524f44f538bbf53ca6
chenlu <chenlu@wepiao.com>
2018-03-22 20:02:10 +0800

    this is master first commit

--- src/main/java/com/eli/Itachi/web/TestController.java
+++ src/main/java/com/eli/Itachi/web/TestController.java
@@ -4,5 +4,6 @@ public class TestController {
 
     public static void main(String[] args) {
         System.out.println("this is master");
+        System.out.println("this is master first commit");
     }
 }
