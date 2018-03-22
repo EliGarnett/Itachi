e1a42421a39256f6b472309f28d727f74f4af98a
chenlu <chenlu@wepiao.com>
2018-03-22 20:27:57 +0800

    dev after rebase first commit

--- src/main/java/com/eli/Itachi/web/DevController.java
+++ src/main/java/com/eli/Itachi/web/DevController.java
@@ -5,6 +5,7 @@ public class DevController {
     public static void main(String[] args) {
         System.out.println("this is dev");
         System.out.println("dev this is dev first commit");
+        System.out.println("dev this is dev second commit");
     }
 
 }
